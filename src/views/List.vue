2393b23bf22bf7e079fff252106174a4e410bbf9
yuan wei <yuanwei92@gmail.com>
2019-08-24 21:39:45 +0800

    added softkey navigation, removed custom list item

--- src/views/List.vue
+++ src/views/List.vue
@@ -3,6 +3,13 @@
     <kai-header title="List" is-fixed></kai-header>
 
     <div class="text is-secondary">
+      Focused one line list item
+    </div>
+    <kai-list>
+      <kai-list-item title="Title"></kai-list-item>
+    </kai-list>
+
+    <div class="text is-secondary">
       One line list item
     </div>
     <kai-list>
@@ -35,46 +42,13 @@
       </kai-list-item>
     </kai-list>
 
-    <div class="text is-secondary mt-2">
-      Focused list item
-    </div>
-    <kai-list>
-      <kai-list-item title="Title" subtitle="Subtitle" is-active>
-        <template #right-icon>
-          <font-awesome-icon
-            style="color: #FFFFFF"
-            class="action-icon-color"
-            icon="chevron-right"
-          />
-        </template>
-      </kai-list-item>
-    </kai-list>
-
-    <div class="text is-secondary mt-2">
-      Custom color list item
-    </div>
-    <kai-list>
-      <kai-list-item
-        title="Title"
-        subtitle="Subtitle"
-        background-color="#cefc6a"
-        title-text-color="#85003e"
-        subtitle-text-color="#00818a"
-      >
-        <template #left-icon>
-          <font-awesome-icon
-            style="font-size: 32px; color: #000000;"
-            class="action-icon-color"
-            icon="user-circle"
-          />
-        </template>
-      </kai-list-item>
-    </kai-list>
+    <kai-software-key right-key="back"></kai-software-key>
   </div>
 </template>
 
 <script>
-import { KaiHeader, KaiList, KaiListItem } from "@/components";
+import { KaiHeader, KaiList, KaiListItem, KaiSoftwareKey } from "@/components";
+import Navigation from "@/js/Navigation";
 
 export default {
   name: "List",
@@ -82,7 +56,44 @@ export default {
   components: {
     KaiHeader,
     KaiList,
-    KaiListItem
+    KaiListItem,
+    KaiSoftwareKey
+  },
+
+  mounted() {
+    Navigation.init();
+    document.addEventListener("keydown", this.onKeyDown);
+  },
+
+  beforeDestroy() {
+    document.removeEventListener("keydown", this.onKeyDown);
+  },
+
+  methods: {
+    onKeyDown(event) {
+      switch (event.key) {
+        case "ArrowDown":
+          document.querySelector("#app").scrollBy({
+            top: 60,
+            behavior: "smooth"
+          });
+          break;
+
+        case "ArrowUp":
+          document.querySelector("#app").scrollBy({
+            top: -60,
+            behavior: "smooth"
+          });
+          break;
+
+        case "SoftRight":
+          this.$router.back();
+          break;
+
+        default:
+          break;
+      }
+    }
   }
 };
 </script>
@@ -92,5 +103,6 @@ export default {
 
 .list {
   padding-top: @header-height;
+  height: calc(~"100vh - 58px");
 }
 </style>
