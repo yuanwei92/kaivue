478347bb7c35c90c185cbbbfab60fabc0335ee20
yuan wei <yuanwei92@gmail.com>
2019-08-24 21:39:03 +0800

    added softkey navigation

--- src/views/Button.vue
+++ src/views/Button.vue
@@ -5,30 +5,63 @@
     <div class="text is-secondary">Default button</div>
     <kai-button>Default</kai-button>
 
-    <div class="text is-secondary">Primary button (Accent color)</div>
-    <kai-button is-primary>Primary</kai-button>
-
-    <div class="text is-secondary">Custom button</div>
-    <kai-button background-color="#00FF00" text-color="#0000FF">
-      Custom
-    </kai-button>
-
     <div class="text is-secondary">Ellipsis</div>
     <kai-button>
       Long long long long long text
     </kai-button>
+
+    <kai-software-key right-key="Back"></kai-software-key>
   </div>
 </template>
 
 <script>
-import { KaiHeader, KaiButton } from "@/components";
+import { KaiHeader, KaiButton, KaiSoftwareKey } from "@/components";
+import Navigation from "@/js/Navigation";
 
 export default {
   name: "Button",
 
   components: {
     KaiHeader,
-    KaiButton
+    KaiButton,
+    KaiSoftwareKey
+  },
+
+  mounted() {
+    Navigation.init();
+    document.addEventListener("keydown", this.onKeyDown);
+  },
+
+  beforeDestroy() {
+    document.removeEventListener("keydown", this.onKeyDown);
+  },
+
+  methods: {
+    onClickListItem(component) {
+      this.$router.push(component.link);
+    },
+
+    onKeyDown(event) {
+      switch (event.key) {
+        case "ArrowDown":
+          Navigation.Down();
+          break;
+
+        case "ArrowUp":
+          Navigation.Up();
+          break;
+
+        case "Enter":
+          break;
+
+        case "SoftRight":
+          this.$router.back();
+          break;
+
+        default:
+          break;
+      }
+    }
   }
 };
 </script>
@@ -38,5 +71,7 @@ export default {
 
 .button {
   padding-top: @header-height;
+  height: calc(~"100vh - 58px");
+  overflow: scroll;
 }
 </style>
--- src/views/Dialog.vue
+++ src/views/Dialog.vue
@@ -5,21 +5,31 @@
     <div class="text is-secondary">
       Basic usage
     </div>
-    <kai-button @click="onShowDialog">
+    <kai-button id="showDialogButton">
       Show Dialog
     </kai-button>
+
     <kai-dialog
       v-if="showDialog"
       title="Info"
       message="This is a normal dialog. Click confirm to show alert."
-      @click-left-key="onClickLeftKey"
-      @click-right-key="onClickRightKey"
     ></kai-dialog>
+
+    <kai-toast v-if="isToastShown" :message="toastMessage" />
+
+    <kai-software-key right-key="Back"></kai-software-key>
   </div>
 </template>
 
 <script>
-import { KaiHeader, KaiDialog, KaiButton } from "@/components";
+import {
+  KaiHeader,
+  KaiDialog,
+  KaiButton,
+  KaiToast,
+  KaiSoftwareKey
+} from "@/components";
+import Navigation from "@/js/Navigation";
 
 export default {
   name: "Dialog",
@@ -27,27 +37,80 @@ export default {
   components: {
     KaiHeader,
     KaiDialog,
-    KaiButton
+    KaiButton,
+    KaiToast,
+    KaiSoftwareKey
   },
 
   data() {
     return {
+      isToastShown: false,
+      toastMessage: "",
       showDialog: false
     };
   },
 
+  mounted() {
+    Navigation.init();
+    document.addEventListener("keydown", this.onKeyDown);
+  },
+
+  beforeDestroy() {
+    document.removeEventListener("keydown", this.onKeyDown);
+  },
+
   methods: {
-    onShowDialog() {
+    onKeyDown(event) {
+      let selectedItem = Navigation.getCurrentItem();
+
+      switch (event.key) {
+        case "SoftLeft":
+          if (this.showDialog) {
+            this.onToggleDialog();
+            break;
+          }
+          break;
+
+        case "SoftRight":
+          if (this.showDialog) {
+            this.showToast("Dialog confirmed!");
+            this.onToggleDialog();
+            break;
+          }
+          this.$router.back();
+          break;
+
+        case "Enter":
+          this.onElementClicked(selectedItem);
+          break;
+
+        default:
+          break;
+      }
+    },
+
+    onToggleDialog() {
       this.showDialog = !this.showDialog;
     },
 
-    onClickLeftKey() {
-      this.showDialog = false;
+    showToast(message) {
+      this.isToastShown = true;
+      this.toastMessage = message;
+      const self = this;
+      setTimeout(function() {
+        self.isToastShown = false;
+      }, 3000);
     },
 
-    onClickRightKey() {
-      alert("Confirmed!");
-      this.showDialog = false;
+    onElementClicked(selectedItem) {
+      switch (selectedItem.element.id) {
+        case "showDialogButton":
+          this.onToggleDialog();
+          break;
+
+        default:
+          break;
+      }
     }
   }
 };
